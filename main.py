a85d7c3f0e32c47963c544dd0eb42f79e66fe05b
Ahmed Ziabat Ziabat <ahmed.ziabat@protonmail.com>
2023-10-26 10:16:28 +0200

    Environment step w/o process replacement

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import json
 import numpy as np
 import gymnasium as gym
 
-from stable_baselines3 import DQN
+
+from stable_baselines3 import DQN, PPO
 
 from mapping_custom_env import CustomRLEnvironment
 
-
-
 # Load the JSON data from the file
-with open('./binomial_4_2.json', 'r') as file:
+with open('./binomial_16_8.json', 'r') as file:
     data = json.load(file)
 
 # Extract the relevant information
@@ -44,22 +43,36 @@ for edge, msg_volume in zip(edges, volume):
 
 # Create the Gym environment with the adjacency matrix and node capacities
 env = CustomRLEnvironment(P, M, np_node_capacity, adj_matrix, n_msgs)
-model = DQN("MlpPolicy", env, verbose=1, learning_starts=5000, device='cpu')
-model.learn(total_timesteps=10000, log_interval=4)
-
-# Example usage:
-obs = env.reset(4)
-done = False
-total_reward = 0
-
-while not done:
-    action = env.action_space.sample()  # Replace this with your RL agent's action selection logic
-    next_obs, reward, done, info, _ = env.step(action)
-    total_reward += reward
-
-print("Total Reward:", total_reward)
 
 
 
+model = DQN(policy="MlpPolicy",
+            env=env,
+            #learning_rate=0.001,
+            learning_starts=5000,
+            tensorboard_log="./tensorboard_16_8/",
+            verbose=1,
+            device='cpu')
+trained = model.learn(total_timesteps=1000000, log_interval=5)
 
 
+terminated = False
+truncated = False
+
+obs, info = env.reset()
+print(obs.shape)
+while not (terminated or truncated):
+    action, _ = trained.predict(observation=obs, deterministic=True)
+    obs, reward, terminated, truncated, info = env.step(action)
+    print("Predict observation:", obs)
+
+placement = {}
+for proc, node in enumerate(obs, start=0):
+    if node not in placement:
+        placement[node] = []
+    placement[node].append(proc)
+
+for node, processes in placement.items():
+    print(f'Processes in node {node}: {processes}')
+
+print("Final assignm", obs)
--- mapping_custom_env.py
+++ mapping_custom_env.py
@@ -1,9 +1,14 @@
 import numpy as np
 import gymnasium as gym
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from gymnasium.spaces import Discrete, MultiDiscrete
 
 DEBUG = False
 
 class CustomRLEnvironment(gym.Env):
+    
     def __init__(self, P, M, node_capacity, adj_matrix, n_msgs):
         super(CustomRLEnvironment, self).__init__()
         self.seed = 0
@@ -15,74 +20,69 @@ class CustomRLEnvironment(gym.Env):
         self.n_msgs = n_msgs
 
         # Initialize all processes unassigned
-        self.current_assignment = np.full(self.P, self.M + 1)
-        self.action_space = gym.spaces.Discrete(self.P * self.M)
+        self.current_assignment = np.full(self.P, (self.M + 1))
+        self.action_space = Discrete(self.P * self.M)
+        self.observation_space = MultiDiscrete([self.M + 2] * self.P)
 
-        self.observation_space = gym.spaces.MultiDiscrete(
-            [self.M + 2 for _ in range(self.P)]
-        )
+        #FIXME observation space es correcto?
+        # self.observation_space = gym.spaces.MultiDiscrete(
+        #     [self.M + 2 for _ in range(self.P)]
+        # )
 
-        # Convert multi-dimensional actions to a single integer
-
-    def reset(self, seed=None):
-        self.current_assignment = np.full(self.P, self.M + 1)
-        # self.node_capacity = self.node_capacity.copy()
+    def reset(self, seed=None, options=None):
+        # FIXME new episode. Should start from last done state or re-init values? 
+        super().reset(seed=seed, options=options)
         self.node_capacity = self.node_capacity_init.copy()  # Reset node capacities
-        return (self.current_assignment.copy()), None
+        self.current_assignment = np.full(self.P, (self.M + 1))
+        return self.current_assignment, {}
 
     def step(self, action):
+
         process_id, node_id = action // self.M, action % self.M
-        node_id = node_id
-        # print("Proceso: ", process_id, " -> nodo: ", node_id)
+        
+        # TODO Truncate when cannot place
+        cannot_place = (self.node_capacity[node_id] == 0) or (self.current_assignment[process_id] != (self.M+1))
 
-        # Check if the node has enough capacity to accommodate the process
-        if self.node_capacity[node_id] > 0:
-            # Check if the process is already assigned to a node and release that capacity
-            if self.current_assignment[process_id] != self.M + 1:
-                old_node_id = self.current_assignment[process_id]
-                # Release the capacity of the previously assigned node
-                self.node_capacity[old_node_id] += 1
-
-            self.current_assignment[process_id] = node_id
-            # Reduce the capacity of the assigned node
+        # If a node cant place a process, reward is 0
+        if cannot_place:
+            reward = 0
+        else:
+            reward = count_communications(self.current_assignment, self.adj_matrix, (self.M+1))
             self.node_capacity[node_id] -= 1
+            self.current_assignment[process_id] = node_id
 
-        # Calculate the total communication volume after this assignment
-        total_volume = 0
-
-        # Calculate the reward (negative total communication volume to minimize it)
-        reward = count_communications(self.current_assignment, self.adj_matrix)
 
         # Check if all processes have been assigned and done with an episode
         full_capacity = np.all(self.node_capacity == 0)
         all_assigned = np.all(self.current_assignment != (self.M + 1))
         done = full_capacity or all_assigned
+
+        info = get_info(action, self.current_assignment, self.node_capacity, reward, done, full_capacity, all_assigned)
         
-
-        info = {"total_volume": total_volume}
-
-
-        if DEBUG:
-            printDebug(action, self.current_assignment, self.node_capacity, reward, done, full_capacity, all_assigned, info)
-        
-        return (self.current_assignment.copy()), reward, done, False, info
+        return self.current_assignment, reward, done, False, info
 
     def render(self, mode="human"):
-        # You can implement rendering the environment if needed
+        # fig, ax = plt.subplots()
+        # nx.draw(self.graph, pos=self.node_positions, with_labels=True, ax=ax)
+        # plt.show()
         pass
-
+        
     def close(self):
         # Clean up resources if needed
         pass
 
 
-def count_communications(positions, adjacency_matrix):
-    # Get the number of processes based on the length of the positions array
-    num_processes = len(positions)
+def count_communications(positions, adjacency_matrix, not_assigned):
+
+    positions_nulled = positions.copy()
+    positions_nulled[positions_nulled==not_assigned] = -1
+    positions_nulled += 1
 
     # Create a mask for positions where processes are different
-    # Assigned to a node?
-    mask = positions[:, None] != positions
+    mask_0 = np.logical_and(positions_nulled[:, None], positions_nulled)
+    mask_1 = positions_nulled[:, None] != positions_nulled
+    
+    mask = mask_0 & mask_1
 
     # Use the mask to filter the adjacency_matrix
     communications_matrix = adjacency_matrix * mask
@@ -90,13 +90,17 @@ def count_communications(positions, adjacency_matrix):
     # Count the number of non-zero elements (corresponding to communications) in the communications_matrix
     communications_count = np.count_nonzero(communications_matrix)
 
-    return communications_count
+    return 1/(communications_count+1)
 
-def printDebug(action, current_assignment, node_capacity, reward, done, full_capacity, all_assigned, info):
-    print("Action:", action)
-    print("Current Assignment:",current_assignment)
-    print("Node Capacities:",node_capacity)
-    print("Reward:", reward)
-    print("Done:", done)
-    print("Done (capacity, assigned):", done + "(" + full_capacity + "," + all_assigned + ")")
-    print("Info:", info)
+def get_info(action, current_assignment, node_capacity, reward, done, full_capacity, all_assigned):
+    return {"Action": action,
+    "Current Assignment": current_assignment,
+    "Node Capacities": node_capacity,
+    "Reward": reward,
+    "Done": done,
+    "Full nodes": full_capacity,
+    "All procs assigned": all_assigned}
+
+
+# G = nx.Graph()
+# G.add_nodes_from
